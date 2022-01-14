abe87663f83651de5d79cbcd27b29d95ddfddb90
Naman Zelawat <namanzelawat@gmail.com>
2022-01-14 22:52:17 +0530

    Added comments

--- concatenateStrings.js
+++ concatenateStrings.js
@@ -1,9 +1,8 @@
 
 /**
  * 
- * @param { string } str1 
- * @param { string } str2 
- * @returns 
+ * @param { String } str1 
+ * @param { String } str2 default null
  */
 function concat(str1, str2 = null)
 {
@@ -23,6 +22,8 @@ function concat(str1, str2 = null)
 /**
  * Test case 1 with both the parameters in same function call
  */
+console.log("\n");
+console.log("--------------  TEST-1 --------------");
 console.log("Taking both parameters");
 var sol1 = concat("hello","world");
 console.log(sol1);
@@ -34,8 +35,10 @@ console.log("\n");
 /**
  * Test case 2 with both the parameters in separate function calls
  */
+ console.log("--------------  TEST-2 --------------");
 console.log("Taking only one parameter");
 var tsol2 = concat("hello");
 console.log("Giving other parameter to the returned function");
 var sol2 = tsol2("world");
 console.log(sol2);
+console.log("\n");
--- separateNumbers.js
+++ separateNumbers.js
@@ -1,14 +1,48 @@
+
+/**
+ * 
+ * @param { Character } char 
+ * @returns { Boolean }
+ */
+function isNumber(char)
+{
+    if(char <= '9' && char >= '0')
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * 
+ * @param { Character } char 
+ * @returns { Boolean }
+ */
+function isCharacter(char)
+{
+    if(!isNumber(char) && char != ' ')
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * 
+ * @param { String } str 
+ * @returns { String }
+ */
 function separate_numbers(str)
 {
     let ans = str[0];
     for(let i = 1;i<str.length;i++)
     {
-        if((str[i] <= '9' && str[i] >= '0') && !(str[i-1] <= '9' && str[i-1] >= '0'))
+        if(isNumber(str[i]) && isCharacter(str[i-1]))
         {
             ans = ans + " ";
             ans = ans + str[i];
         }
-        else if(!(str[i] <= '9' && str[i] >= '0') && (str[i-1] <= '9' && str[i-1] >= '0'))
+        else if(isCharacter(str[i]) && isNumber(str[i-1]))
         {
             ans = ans + " ";
             ans = ans + str[i];
@@ -21,8 +55,48 @@ function separate_numbers(str)
     return ans;
 }
 
+console.log("\n");
+/**
+ * Test case 1
+ */
+console.log("--------------  TEST-1 --------------");
 let str_test1 = "call me at9123457";
-console.log(separate_numbers(str_test1));
+console.log("Input -- " + str_test1);
+console.log("Output -- " + separate_numbers(str_test1));
+console.log("\n");
 
+/**
+ * Test case 2
+ */
+console.log("--------------  TEST-2 --------------");
 let str_test2 = "9ft tall building 3700parkavenue,NY";
-console.log(separate_numbers(str_test2));
+console.log("Input -- " + str_test2);
+console.log("Output -- " + separate_numbers(str_test2));
+console.log("\n");
+
+/**
+ * Test case 3
+ */
+ console.log("--------------  TEST-3 --------------");
+ let str_test3 = "915ft tall building3700parkavenue,NY";
+ console.log("Input -- " + str_test3);
+ console.log("Output -- " + separate_numbers(str_test3));
+ console.log("\n");
+
+ /**
+ * Test case 4
+ */
+console.log("--------------  TEST-4 --------------");
+let str_test4 = "call me at9123457, tonight8pm";
+console.log("Input -- " + str_test4);
+console.log("Output -- " + separate_numbers(str_test4));
+console.log("\n");
+
+ /**
+ * Test case 5
+ */
+  console.log("--------------  TEST-5 --------------");
+  let str_test5 = "8pm call me at9123457, tonight before9";
+  console.log("Input -- " + str_test5);
+  console.log("Output -- " + separate_numbers(str_test5));
+  console.log("\n");
